c5d604a69e663517e1cfc3ee0b651228d72e298b
sbm-ocampagnoli <otaviocampagnoli@hotmail.com>
2024-05-27 14:31:14 -0300

    Adicionando sons no jogo.

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
-var xBolinha = 100;
-var yBolinha = 100;
-var diametro = 15;
+var xBolinha = 50;
+var yBolinha = 50;
+var diametro = 13;
 let raio = diametro / 2;
 
 let velocidadeXBolinha = 6;
@@ -20,8 +20,17 @@ let velocidadeYOponente;
 let meusPontos = 0;
 let pontosDoOponente = 0;
 
+let raquetada, ponto, trilha;
+
+function preload() {
+    trilha = loadSound("./trilha.mp3");
+    raquetada = loadSound("./raquetada.mp3");
+    ponto = loadSound("./ponto.mp3");
+}
+
 function setup() {
     createCanvas(600, 400);
+    trilha.loop();
 }
 
 function draw() {
@@ -87,6 +96,7 @@ function verificarColisaoRaquete(x, y) {
     colidiu = collideRectCircle(x, y, comprimentoDaRaquete, alturaDaRaquete, xBolinha, yBolinha, raio);
     if (colidiu) {
         velocidadeXBolinha *= -1;
+        raquetada.play();
     }
 }
 
@@ -96,16 +106,26 @@ function movimentarRaqueteDoOponente() {
 }
 
 function incluirPlacar() {
-    fill(255)
-    text(meusPontos, 278, 26);
-    text(pontosDoOponente, 321, 26);
+    stroke(255);
+    textAlign(CENTER);
+    textSize(16);
+    fill(color(255, 140, 0));
+    rect(150, 10, 40, 20);
+    fill(255);
+    text(meusPontos, 170, 26);
+    fill(color(255, 140, 0));
+    rect(450, 10, 40, 20);
+    fill(255);
+    text(pontosDoOponente, 470, 26);
 }
 
 function marcarPonto() {
     if (xBolinha > 590) {
         meusPontos += 1;
+        ponto.play();
     }
     else if (xBolinha < 10) {
         pontosDoOponente += 1;
+        ponto.play();
     }
 }
